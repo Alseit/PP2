1bbee1bc05d2e78c4cbab58a28144465db57f197
Alseit <alseit.maksut@mail.ru>
2019-02-13 16:07:31 +0600

    PP2

--- Week 1/Task 2/Task 2/Program.cs
+++ Week 1/Task 2/Task 2/Program.cs
@@ -8,16 +8,19 @@ namespace Task_2
 {
     class Program
     {
+        // Создаем класс Student и вводим туда имя, ID и год обучения
         class Student
         {
             public string name;
             public string ID;
             public int year;
+            // Создаем конструктор Student в котором содержится имя и ID 
             public Student(string name, string ID)
             {
                 this.name = name;
                 this.ID = ID;
             }
+            // Создаем функцию которая выводит все элементы класса Student и увеличивает год обучения на 1 год
             public void Print()
             {
                 Console.WriteLine("Student's name: " + this.name + Environment.NewLine + "Student's ID: " + this.ID + Environment.NewLine + "Student's year of study: " + (year + 1));
@@ -25,9 +28,10 @@ namespace Task_2
         }
         static void Main(string[] args)
         {
-            Student Alseit = new Student("Alseit", "18BD110636");
-            Alseit.year = 1;
-            Alseit.Print();
+            // Создаем переменную а типа Student
+            Student a = new Student("Alseit", "18BD110636");
+            a.year = 1;
+            a.Print();
             Console.ReadKey();
         }
     }
---
+++ Week 1/Task 4/Task 4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Вводим число
+            int n = int.Parse(Console.ReadLine());
+            // Создаем два цикла где итератор второго цикла не может превышать итератор первого цикла
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j <= i; j++)
+                {
+                    // Пока второй цикл работает мы выводим звездочку в один ряд
+                    Console.Write("[*]");
+                }
+                // Когда второй цикл заканчивается мы переносим следующую звездочкуна следующую строку
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
