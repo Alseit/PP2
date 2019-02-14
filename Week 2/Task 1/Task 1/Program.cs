93c8735a541f3efe0e321a67564dd1609ea4323e
Alseit <alseit.maksut@mail.ru>
2019-02-14 23:37:17 +0600

    labs

---
+++ Week 2/Task 1/Task 1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        /// <summary>
+        /// Главная точка входа в проект
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            // Создаем переменную Pol типа bool
+            bool Pol = true;
+
+            // Считываем данные из файла
+            using (FileStream fs = new FileStream(@"C: \Users\АПИТУ\Desktop\PP2\Week 2\Task 1\input.txt", FileMode.Open, FileAccess.Read))
+            {
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    // Создаем переменную text типа string и сохраняем все элементы файла в texte 
+                    // После эту переменную проверяем через типа bool на палиндромность и записываем ответ а переменную типа bool
+                    // Если text палиндром то true, а если не палиндром то false 
+                    string text = sr.ReadLine();
+                    Pol = Polindrome(text);
+                    if(Pol == true)
+                    {
+                        Console.WriteLine("Yes");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No");
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Проверка на Палиндромность
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        private static bool Polindrome(string text) 
+        {
+            // Создаем цикл чтобы пробежаться по элементам массива типа char
+            for(int i = 0; i < (text.Length) / 2; i++)
+            {
+                // Здесь через условие проверяем 1 элемент массива и последний элемент массива
+                // Если они одинаковые то переходим на следующий элемент
+                // А если не одинаковые то выводим false и выходим из функции
+                if(text[i] != text[text.Length - 1 - i])
+                {
+                return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Week 2/Task 2/Task 2/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создаем массив простых чисел
+            int[] PrimeNumbers;
+
+            // Считываем данные из файла
+            using (FileStream fs = new FileStream(@"C:\Users\АПИТУ\Desktop\PP2\Week 2\Task 2\Input.txt", FileMode.Open, FileAccess.Read))
+            {
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    // Фильтр чисел
+                    string text = sr.ReadLine();
+                    int[] numbers = Numbers(text);
+                    PrimeNumbers = IsPrime(numbers);
+                }
+            }
+            // Записываем результат на другой файл
+            using (FileStream fs2 = new FileStream(@"C:\Users\АПИТУ\Desktop\PP2\Week 2\Task 2\Output", FileMode.OpenOrCreate, FileAccess.Write))
+            {
+                using (StreamWriter sw = new StreamWriter(fs2))
+                {
+                    // Запись простых чисел через пробел
+                    foreach (var x in PrimeNumbers)
+                    {
+                        sw.Write(x + " ");
+                    }
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Проверка на простые числа
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        private static int[] IsPrime(int[] num)
+        {
+            List<int> res = new List<int>();
+
+            foreach (var x in num)
+            {
+                int y = 0;
+                for(int i = 2; i * i <= x; i++)
+                {
+                    if(x % i == 0)
+                    {
+                        y++;
+                    }
+                }
+                if (y == 0)
+                {
+                    res.Add(x);
+                } 
+            }
+            return res.ToArray();
+        }
+
+        /// <summary>
+        /// Превращение входных данных в массив чисел
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        private static int [] Numbers(string text)
+        {
+            string[] parts = text.Split();
+            int[] res = new int[parts.Length];
+            for (int i = 0; i < parts.Length; ++i)
+            {
+                res[i] = int.Parse(parts[i]);
+            }
+            return res;
+        }
+    }
+}
