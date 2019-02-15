dc8d560655a260334a4ea0b9fe163f4abc6031d8
Alseit <alseit.maksut@mail.ru>
2019-02-15 23:30:26 +0600

    Labs

--- Week 2/Task 3/Task 3/Program.cs
+++ Week 2/Task 3/Task 3/Program.cs
@@ -11,11 +11,13 @@ namespace Task_3
     {
         static void Main(string[] args)
         {
+            // Создаем DirectoryInfo dir и открываем к ней доступ через ссылку
             DirectoryInfo dir = new DirectoryInfo(@"C:\Users\АПИТУ\Desktop\PP2\Week 2");
+            // Вызываем функцию
             PrintInfo(dir, 0);
             Console.ReadKey();
         }
-
+        // Через этот метод пишем названия папок и файлов, и то что внутри папок пишем через 5 пробелов на следующей строке и тд
         static void PrintInfo(FileSystemInfo fsi, int k)
         {
             string s = new string(' ', k);
